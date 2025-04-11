1cbf0ba7eb07170f13c47bb2f005526a3c2b6c51
Orlando A Karam <orlando.karam@gmail.com>
2025-04-11 18:32:33 -0400

    Adding earmark and validation

--- simple_blog/lib/simple_blog_web/controllers/article_html.ex
+++ simple_blog/lib/simple_blog_web/controllers/article_html.ex
@@ -1,5 +1,12 @@
 defmodule SimpleBlogWeb.ArticleHTML do
   use SimpleBlogWeb, :html
 
+  def md_to_html(md) do
+    case Earmark.as_html(md) do
+      {:ok, html, _messages} -> {:safe, HtmlSanitizeEx.basic_html(html)}
+      {:error, html, _messages} -> {:safe,"Errors converting markdown"}
+    end
+  end
+
   embed_templates "article_html/*"
 end
--- simple_blog/mix.exs
+++ simple_blog/mix.exs
@@ -58,7 +58,9 @@ defmodule SimpleBlog.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:earmark, "~> 1.4.47"},
+      {:html_sanitize_ex, "~> 1.4"}
     ]
   end
 
